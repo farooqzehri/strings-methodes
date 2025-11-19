7c34f41de987b1b2e696895d9195fb7c20c53bd0
farooqzehri <farooqahmedbaloch17@gmail.com>
2025-11-19 14:20:00 +0500

    task 3

---
+++ app.js
@@ -0,0 +1,26 @@
+// 1. Lowercase Name 
+// Write a function that takes a name as input and returns it all in lowercase.
+
+function UpperCaseName(name){
+    return name.toLowerCase();
+}
+console.log(UpperCaseName("FAROOQ ZEHRI"));     // ok
+
+
+// 2. Uppercase Shouting 
+// Convert a given string into uppercase to simulate shouting. Example: "hello" → 
+// "HELLO".
+
+function shout(text){
+    return text.toUpperCase();
+}
+console.log(shout("hello"));   // ok
+
+//3. Count Characters 
+// Given a string, return the number of characters in it.
+//
+
+let str = "Hello-world";
+console.log(str.length);   // task 3
+
+
